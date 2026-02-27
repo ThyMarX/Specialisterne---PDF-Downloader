433e50cfdafe36f18e9d19fad87296382e6cc56d
ThyMarX <fake.dk@hotmail.com>
2026-02-27 14:45:53 +0100

    Version 1
    It's got everything except a UML diagram

---
+++ README.md
@@ -0,0 +1,62 @@
+# Specialisterne---PDF Downloader
+
+A python script to download a bunch of pdf files based on a .xlsx sheet.
+
+## Description
+
+The script ("downloadPDF.py"), when run correctly, consist of 4 parts:
+    -Step 1: Extracting "BRnum", "link 1" and "link 2" from every row in a excel sheet "GRI_2017_2020 (1).xlsx" and appeneding them as a list into pdf_files[].
+    -Step 2: Creating a new uniquely named folder "Downloaded pdfsX".
+    -Step 3: For each item in pdf_files[], try to download the pdf from "link 1", and then from "link 2" if it failed, into the new folder. Note in item[0] whether it was downloaded (yes) or wasn't (no).
+    -Step 4: Make a .cvs report with all the info from pdf_files[].
+
+The project was given to me by "Specialisterne - Academy".
+In the project we were given an old python script that didn't complete the task optimally, so we were told to either improve it, or make our own solution.
+I didn't know enough about python to optimize it, so i made my own from scratch.
+
+The files in the "\other files\" folder are as follows:
+    -"downloadPDF copy.py": A slightly older version of the script (but with the same full functionality) that has the authors old print() lines for testing.
+    -"Kravspecifikation.pdf": A requirement specification for the project.
+    -"Metadata2006_2016.xlsx": A uneeded file given as part of the project files.
+    -"OLDdownload_files.py": The old code that was given as part of the project files.
+    -"PDF Downloader (Opgavebeskrivelse).pdf": The full project description.
+
+OBS!:
+The script is so far only made to handle excel sheets, and only download pdf files.
+Currently you have to manually alter 2 lines in the code if you want to make the script download files from another excel sheet.
+The script has only been tried on a windows 11 computer, and only through VS Code
+I didn't have time to make a UML Diagram, but hopefully this readme file and the comments in the python file is descriptive enough.
+
+## Getting Started
+
+### Dependencies
+
+* You need python and the following packages installed: pandas and requests
+
+### Installing
+
+* Download the "downloadPDF.py" and the "GRI_2017_2020 (1).xlsx" file into the same folder on your computer.
+
+### Executing program
+
+* Make sure that "downloadPDF.py" and "GRI_2017_2020 (1).xlsx" are located in the same folder.
+    * (If you want the script to handle a different excel sheet, then you must manually alter line 23 and 35 in the code)
+* Run "downloadPDF.py" (ideally by opening it in VS Code and pressing F5).
+* Wait for the program to finish (can take up to 30 minutes), see the terminal for progress messages.
+* When finished, open the new \Downloaded pdfsX\ folder, check it includes PDF files.
+* Open the "Report.cvs" file (in excel, sheet, notepad, etc.) and see that the script has noted which files has and hasn't been downloaded.
+
+That's it!
+
+## Help
+
+To make sure the script doesn't run it's main function when run, and instead is ready for testing, change the str parameter from "__main__" on line 120 to something else. (Make sure you change it back whne you need to run it properly.)
+
+The authors test print() lines has been removed from the current script so as not to clutter the script.
+If need inspiration on where to put your own print() lines to get relevent test information, then see the a slightly older version (but with the same full functionality) of the script here: "\other files\downloadPDF copy.py".
+
+In the bottom of the script there are some test code and functions, as well as a note of possible future improvements to the code.
+
+## Authors
+
+Thomas Dyrholm Siemsen
---
+++ downloadPDF.py
@@ -0,0 +1,167 @@
+import os
+import pandas
+import requests
+import csv
+
+#   ---
+#
+#   Hello user
+#
+#   Here's how to change the script to use it with an excel sheet that isn't "GRI_2017_2020 (1).xlsx":
+#   
+#       -1 On line 23, where inputFiles is declared and change the str parameter in .read_excel() to the full name and file type of the new sheet.
+#       -2 In listHandling(), on line 35 where pdf_files[] is being appended and change the name of 'BRnum', 'Pdf_URL' and 'Report Html Address' to match the new names of the subesquent rows ('file name', 'link 1', 'link 2').
+#
+#   Remember to read the readme file before running the script!
+#
+#   Good luck! :)
+#   -Thomas
+#  
+#   ---
+
+
+inputFiles = pandas.read_excel("GRI_2017_2020 (1).xlsx")        #   Get the excel file
+pdf_files = []          #   The list where we make a new list for every file [["was_downloaded_status", "BRnum", "link1", "link2"],...]
+folderNr = 1            #   A int to make sure we can create a uniquely named folder
+downloadedNr = 0        #   Total number of succesfully downloaded files
+notDownloadedNr = 0     #   Total number of files that wasn't downloaded
+rowsProcesedNr = 0      #   Total number of rows that has been processed to keep the user updated while downloading
+
+
+# ---Step 1: Converts the excel sheet into a python list[]
+def listHandling():
+    global pdf_files
+    max = 0     #   when testing with max nr of files
+    for index, row in inputFiles.iterrows():    #   For loop for each row of data in the .xslx file. I don't fully understand how this works, but it does ¯\_(ツ)_/¯ and the seemingly unused 'index' variable is important to declare because of iterrows() https://stackoverflow.com/questions/16476924/how-can-i-iterate-over-rows-in-a-pandas-dataframe
+        pdf_files.append(["", row['BRnum'], row['Pdf_URL'], row['Report Html Address']])    #   Add the importat parts of the row as a new list to pdf_files[] 
+        max += 1     #  when testing with max nr of files
+        if max == 12:        #  when testing with max nr of files, set nr here
+           break
+
+
+# ---Step 2: Makes a folder to put the files
+def createDir():        
+    global folderNr
+    try:
+        path = 'Downloaded pdfs'+str(folderNr)
+        os.makedirs(path)
+    except FileExistsError:     #   if the directory already exists, try again
+        folderNr += 1
+        createDir()
+
+
+# ---Step 3: Downloads the files into the folder and tracks progress
+def downloadAllPDFS():     
+    global pdf_files
+    global downloadedNr
+    global notDownloadedNr
+    for item in pdf_files:
+        downloadOnePdf(item, 1)     #   Splits the download functions as to not repeat code and so you can more easily test one row at a time
+    print("\nDone with downloading.\nFiles succusfully downloaded: "+str(downloadedNr)+" | Files failed to be downloaded: "+str(notDownloadedNr))
+
+# -Download a file based on a specific row from the excel sheet
+def downloadOnePdf(item, attempt):
+    global downloadedNr
+    global notDownloadedNr
+    global folderNr
+    global rowsProcesedNr
+    error = 0       #   Error checker
+
+    if attempt == 1:            #   First or second attempt = first or second link
+        link = item[2]
+    elif attempt == 2:
+        link = item[3]
+    
+    _, file_extension = os.path.splitext(str(link))        #    link = PDF?, get the file extension (aka ".pdf")
+
+    try:                                        #   If the program can download the file
+        r = requests.get(link,timeout=3)
+        r.raise_for_status()
+        
+        file_Path = 'Downloaded pdfs'+str(folderNr)+'/'+item[1]+'.pdf'
+        
+        if file_extension == ".pdf":                      #    link = PDF?, might not be good enough (see BR50051)?
+            if r.status_code == 200:
+                with open(file_Path, 'wb') as file:
+                    file.write(r.content)
+                item[0] = "yes"
+                downloadedNr += 1
+                rowsProcesedNr += 1
+        else:       #   If it's not a pdf, but the request still goes through ()
+            error = 1
+
+    except requests.exceptions.RequestException as err:     #   if the program can't download the file
+        #print ("File "+item[1]+" error: ",err)     #   test error message, not needed for final output
+        error = 1
+
+    if error == 1:      #   If something went wrong, one way or another
+        if attempt == 1:
+            downloadOnePdf(item, 2)
+        if attempt == 2:
+            item[0] = "no"
+            notDownloadedNr += 1
+            rowsProcesedNr += 1
+
+    if rowsProcesedNr % 1000 == 0:      #   Progress message
+        print('\n Status: The program has processed a total of '+str(rowsProcesedNr)+' rows in the excel sheet so far...')
+
+
+# ---Step 4: Creates final report of every file, and a summary, into the new folder
+def createReport():     
+    global pdf_files
+    global folderNr
+
+    with open('Downloaded pdfs'+str(folderNr)+'/Report.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(pdf_files)
+
+
+# ---What the script runs:
+if __name__ == "__main__":      #   Change  the str parameter from "__main__" to not run the following code for test purposes
+    print('\nConverting the excel sheet into a list for easier handling...')
+    listHandling()
+    print('\nCreating a folder to place the downloaded files and report...')
+    createDir()
+    print('\nDownloading files...')
+    downloadAllPDFS()
+    print('\nCreating the report...')
+    createReport()
+    print('\nThe program is done! :)\nFind your files and the report.cvs in the "Downloaded pdfs'+str(folderNr)+'" folder.')
+
+
+
+
+
+
+# ---Space for test functions (remember to change "__main__" to something else)
+
+# -When testing downloadOnePdf(), make sure you don't already have a "Downloaded pdfs1" folder.
+#testRow = ["",	"BR50050", "https://www.akb.ch/documents/30573/92310/nachhaltigkeitsbericht-2016.pdf", "https://www.akb.ch/die-akb/kommunikation-medien/geschaeftsberichte"]
+#downloadOnePdf(testRow, 1)
+#print(testRow)
+
+# -Usuable exceptions for the try part of downloadOnePdf() 
+''' 
+    #   The following code is from https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+
+    except requests.exceptions.HTTPError as errh:       
+        print ("File"+item[1]+" Http Error:",errh)
+        error = 1
+    except requests.exceptions.ConnectionError as errc:
+        print ("File"+item[1]+" Error Connecting:",errc)
+        error = 1
+    except requests.exceptions.Timeout as errt:
+        print ("File"+item[1]+" Timeout Error:",errt)
+        error = 1
+    except requests.exceptions.TooManyRedirects as errr:
+        print ("File"+item[1]+" Too many redirects Error:",errr)
+        error = 1
+'''
+
+
+# Future improvements
+#   -I could optimize the speed by not putting the excel data into a list, and just download right away in listHandling() (also by somehow download multiple files at once but i don't know how to do that?)
+#   -Mention in the readme and as a print how long the program might take pr 1000 rows or 1000 downloads (figure out how long)
+#   -Make the script tell the user how many rows there are in the excel sheet
+#   -Make a better file type checker (see BR50051)
+#   -Make a checker to see if the file is actually a excel sheet
